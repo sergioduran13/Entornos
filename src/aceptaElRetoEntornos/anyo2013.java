af8e1795d59b12492ab5bce3c2af1689c9c4c0e3
Sergio Duran <sergiodurancazorla@gmail.com>
2020-02-27 20:35:42 +0100

    Anyo2013 empezado. Metodo que devuelve si es repetido. Falta busqueda de todos los anyos y mostrar resultado.

--- src/aceptaElRetoEntornos/anyo2013.java
+++ src/aceptaElRetoEntornos/anyo2013.java
@@ -4,7 +4,56 @@ import java.util.Scanner;
 
 public class anyo2013 {
 	
+	public static void main(String[] args) {
+		Scanner tec = new Scanner(System.in);
+		
+		int casos = tec.nextInt();
+		
+		for (int i = 0; i < casos; i++) {
+			
+			int anyo = tec.nextInt();
+			int serie;
+			int diferencia;
+			
+			//Busqueda de valores repetidos
+			String anyoBusqueda =  Integer.toString(anyo);
+			char[] arrayAnyo = new char [4];
+			
+			//Rellenar el array
+			
+			for (int j = 0; j < arrayAnyo.length; j++) {
+				arrayAnyo[j] = anyoBusqueda.charAt(j+1);
+			}
+			
+			//Busqueda de anyo repetidos:
+			
+			if(numeroRepetido(arrayAnyo)){			
+				
+			}
+			
+			
+		}
+		
+	}
+
 	
+	public static boolean numeroRepetido(char[] anyo){
+		//Metodo que devuelve true si tiene un numero repetido. False si no tiene repetido.
+		
+		boolean resultado = false;
+		
+		//Recorrido
+		for (int i = 0; i < anyo.length && !resultado; i++) {		
+			//Busqueda
+				for (int j = 1; j < anyo.length && !resultado; j++) {
+					if(anyo[i] == anyo[j] )
+						resultado = true;
+				}
+		}
+		
+		
+		return resultado;
+	}
 
 }
 
